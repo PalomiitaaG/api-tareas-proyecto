f08a0b1bf2b61d29982a032c9f4fb9e68ed6c5d0
Paloma <palomagutierrez947@gmail.com>
2024-04-02 18:43:04 +0200

    backen terminado

--- db.js
+++ db.js
@@ -7,12 +7,13 @@ function conectar(){
     return MongoClient.connect(process.env.URL_MONGO);
 }
 
-
+//creamos una funcion para leer las tareas
 function getTareas(){
     return new Promise(async (ok,ko) => {
         try{
             const conexion = await conectar();
-
+            
+            //hacemos la conexion a la base de datos y donde tiene que coger la información.
             let coleccion = conexion.db("tareas").collection("tareas");
 
             //creamos una variable para que meta dentro todas las careas encontradas convertidas en un array.
@@ -29,6 +30,7 @@ function getTareas(){
     });
 }
 
+//creamos una funcion para crear las tarminadas, además de al crearlas coloca el toggle como no terminada
 function crearTarea(tarea){
     return new Promise(async (ok,ko) => {
         try{
@@ -43,23 +45,25 @@ function crearTarea(tarea){
 
             conexion.close();
 
-            //aqui le decimos que nos muestre un objeto con id : "id de la tarea"
+            //si se cumple la promesa nos muestre un objeto con id : "id de la tarea"
             ok({id : insertedId});
 
         }catch(error){
-            console.log(error);
+
             ko({error : "error en base de datos"});
         }
     });
 }
 
-function borrarTarea(id){
+//creamos la funcion para borrar las tareas
+function borrarTarea(id){ //le pasamos el id de la tarea que querramos borrar
     return new Promise(async (ok,ko) => {
         try{
             const conexion = await conectar();
 
             let coleccion = conexion.db("tareas").collection("tareas");
 
+            //Con deleteOne borramos un elemento en especifico y lo metemos en un objeto que seria 1 o 0. 1-->(se borró), 2 -->(ya esta eliminada)
             let {deletedCount} = await coleccion.deleteOne({ _id : new ObjectId(id) });
 
             conexion.close();
@@ -73,15 +77,22 @@ function borrarTarea(id){
     });
 }
 
-function actualizarEstado(id,){
+//creamos una función para actualizar el estado.
+function actualizarEstado(id){
     return new Promise(async (ok,ko) => {
         try{
             const conexion = await conectar();
 
             let coleccion = conexion.db("tareas").collection("tareas");
-            
-            //preguntar, primero hay qye poner terminado y luego no terminar, primero hacer el finndone y con el valor pongo el not terminado.
-            //let resultado = await coleccion.updateOne({  },{$set : });
+
+            //buscamos la tarea através del id
+            let tarea = await coleccion.findOne({_id : new ObjectId(id)});
+
+            //creamos una variable para decir que la tarea esta terminada
+            let nuevoEstado = !tarea.terminada;
+
+            //actualizamos el estado de la tarea.
+            let resultado = await coleccion.updateOne({_id : new ObjectId(id)}, {$set : {terminada : nuevoEstado}});
 
             conexion.close();
 
@@ -92,6 +103,7 @@ function actualizarEstado(id,){
     });
 }
 
+//creamos  una función para actualizar el texto.
 function actualizarTexto(id,tarea){
     return new Promise(async (ok,ko) => {
         try{
@@ -99,20 +111,17 @@ function actualizarTexto(id,tarea){
 
             let coleccion = conexion.db("tareas").collection("tareas");
             
-            //preguntar, primero hay qye poner terminado y luego no terminar
-            let resultado = await coleccion.updateOne({_id : new ObjectId(id)},{ $set : {tarea : tarea }});
+            //actualizamos el texto con la nueva tarea.
+            let actualizar = await coleccion.updateOne({_id : new ObjectId(id)},{ $set : {tarea : tarea }});
 
             conexion.close();
 
-            ok(resultado); 
+            ok(actualizar); 
         }catch(error){
             ko({error : "error en base de datos"});
         }
     });
 }
-
-//actualizarTexto('65f1e7ca1b101672010488ee',"nueva tarea")
-//.then(algo => console.log(algo))
 
 
 module.exports = {getTareas,crearTarea,borrarTarea,actualizarEstado,actualizarTexto}
--- index.js
+++ index.js
@@ -1,63 +1,93 @@
 require("dotenv").config();
 const express = require("express");
-const {getTareas,crearTarea,borrarTarea,actualizarTarea,actualizarEstado,actualizarTexto} = require("./db");
+const {getTareas,crearTarea,borrarTarea,actualizarEstado,actualizarTexto} = require("./db");
 const {json} = require("body-parser");
 const cors = require("cors");
 
 const servidor = express();
 
-servidor.use(json());
+servidor.use(cors());
+servidor.use(json()); //cualquier cosa que venga en json es procesado por el body-parse.
 
 //Para comprobar si funciona la conexion
 servidor.use("/mentirillas", express.static("./pruebas_api"));
 
+//midleware para las solicitudesde de GET
 servidor.get("/api-todo", async (peticion,respuesta) => {
     try{
+        //esperamos la llegada de la respuesta de la funcion de get de la bbdd.
         let tareas = await getTareas();
-        respuesta.json(tareas.map(({_id,tarea}) => { return {id : _id,tarea}})); // colocamos el return y las llaves al rededor del ojbeto para que no piese que sigue la función.ss
+
+        //hacemos map para que nos devuelva un objeto se que llamara id, que dentro tendra la propiedad del id y de terminada.
+         tareas = tareas.map(({_id,tarea,terminada}) => { return {id : _id,tarea,terminada}});
+
+        respuesta.json(tareas); // colocamos el return y las llaves al rededor del ojbeto para que no piese que sigue la función.ss
     }catch(error){
         respuesta.status(500);
         respuesta.json(error);
     }
 });
-
+ 
+//creamos un middleware para las solicutudes POST 
 servidor.post("/api-todo/crear", async (peticion,respuesta,siguiente) => {
-    /*let {tarea} = peticion.body;
-
-    if(tarea && tarea.trim() != ""){
-        try{
-            let id = await crearTareas({tarea});
-
-            return respuesta.json({id})
-        }catch(error){
-            respuesta.status(500);
-            respuesta.json(error);
-        }
-    }
-    siguiente({error : "falta el argumento tarea en el objeto JSON"});*/
+    //extramos la tarea del cuerpo de la petición
     let {tarea} = peticion.body;
-
+    
+    //si la tarea es verdadero además de tarea sin ningun espacio es distinto a vacio hace el try.
     if(tarea && tarea.trim() != ""){
         try{
+            //espera la respuesta de la funcion crearTarea de la bbdd.
             let id = await crearTarea({tarea});
-            return respuesta.json(id);
+
+            //responde con el id de la nueva tarea.
+            return respuesta.json({id});
         }catch(error){
             respuesta.status(500);
             return respuesta.json(error);
         }
     }
-
+    //si noy una tarea valida, por ejemplo si se ha equivocado en el formato de la solicitud.
     siguiente({error : "falta la tarea"});
 });
 
-servidor.put("/api-todo/actualizar/:id/:operacion", (peticion,respuesta) => {
-    respuesta.send("...put");
+//creamos un middleware para las solicitudes PUT. En el que hemos puesto una expresion regular en el que puede contener cualquier letra y número además de ser 24 caracteres.
+servidor.put("/api-todo/actualizar/:id([a-f0-9]{24})/:operacion(1|2)", async(peticion,respuesta) => {
+    //el id que ha puesto el usuario lo combierto en numeros y lo guardo en la variable operacion
+    let operacion = Number(peticion.params.operacion);
+
+    //las operaciones equivalen a 1 o 2
+    let operaciones = [actualizarTexto,actualizarEstado];
+
+    //la peticion body te va dar 1 o 2. Si es 1 --> actualizar texto, si es 2 --> actualizar estado.
+    let {tarea} = peticion.body;
+
+    //si la operacion es 1 directamente te devuele el siguiente.
+    if(operacion == 1 && (!tarea || tarea.trim() == "")){
+        return siguiente({ error : "falta el argumento tarea en el objeto JSON" });
+    }
+    
+    try{
+        // si en la operacion entre 1, hace operacion 1 -1 igual a 0 que es el indice 0 = actulizarTexto, y si en la operacion es 2 hace 2- 1 = 1 = actualizarEstado
+        let cantidad = await operaciones[operacion - 1](peticion.params.id, operacion == 1 ? tarea : null);
+
+        respuesta.json({ resultado : cantidad ? "ok" : "ko"});
+
+    }catch(error){
+
+        respuesta.status(500);
+        respuesta.json(error);
+    }
 });
 
+//creamos un middleware para las solicitudes para borrar.
 servidor.delete("/api-todo/borrar/:id([a-f0-9]{24})", async (peticion,respuesta) => {
+    //el id tiene una expresion regular que lo que dice es que puede ser cualquier letra o numero, en total tienen que ser 24 caracteres.
+
     try{
+        //esperamos a la funcion de borrar pasandole el parametro del id de la peticion
         let cantidad = await borrarTarea(peticion.params.id);
 
+        //si la cantidad es 1 es que ha sido borrado y si sale 0 no se ha podido eliminar.
         respuesta.json({resultado : cantidad > 0 ? "ok" : "ko"});
     }catch(error){
         respuesta.status(500);
